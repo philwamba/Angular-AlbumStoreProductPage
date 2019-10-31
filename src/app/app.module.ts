a673c0dbd65ac666d0310337f2b487dc85d6a30a
Phil Wamba <philwamba@gmail.com>
2019-10-31 11:59:43 +0300

    Create ProductService Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
-    ProductDescriptionComponent
+    ProductDescriptionComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
