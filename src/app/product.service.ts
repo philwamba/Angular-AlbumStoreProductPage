523b4edfbae7f2c959c655d2b929de546e21d394
Phil Wamba <philwamba@gmail.com>
2019-10-31 12:21:21 +0300

    Implement getAlbum method

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,13 @@
 import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-
+  private _albumUrl: string = '../assets/album.json';
   constructor(private _http: Http) { }
 
+  getAlbum(id:number) {
+    return this._http.get(_albumUrl);
+  }
 }
